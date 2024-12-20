c3b945b3154fec4309a5d67d7c4d91abd6f8f1a7
leote <leotep34@gmail.com>
2024-12-20 16:03:07 +0100

    initialisation config rabbitmq

---
+++ RabbitMQConfig/src/main/java/epsi/mspr814/kls/rabbitmqconfig/RabbitMqConfigApplication.java
@@ -0,0 +1,13 @@
+package epsi.mspr814.kls.rabbitmqconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitMqConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitMqConfigApplication.class, args);
+    }
+
+}
