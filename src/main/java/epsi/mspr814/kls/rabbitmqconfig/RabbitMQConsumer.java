c47e3cb1082a7a91e757cdce21eabcedf0b263d3
Kickleo <leotep34@gmail.com>
2025-01-09 10:20:42 +0100

    First RabbitMQ tests with spring

---
+++ src/main/java/epsi/mspr814/kls/rabbitmqconfig/RabbitMQConsumer.java
@@ -0,0 +1,13 @@
+package epsi.mspr814.kls.rabbitmqconfig;
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RabbitMQConsumer {
+
+    @RabbitListener(queues = RabbitMQConfig.QUEUE_NAME)
+    public void listen(String message) {
+        System.out.println("Received message: " + message);
+    }
+}
